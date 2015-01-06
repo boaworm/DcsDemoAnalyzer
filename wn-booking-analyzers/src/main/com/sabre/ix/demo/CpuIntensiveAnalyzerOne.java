edc8790ec7276aafea6d55823ba163a39abfa0c4
boaworm <boaworm@gmail.com>
2015-01-06 15:45:49 -0600

    Adding six implementations

---
+++ wn-booking-analyzers/src/main/com/sabre/ix/demo/CpuIntensiveAnalyzerOne.java
@@ -0,0 +1,9 @@
+package com.sabre.ix.demo;
+
+/**
+ * Creator: Henrik Thorburn (sg0211570)
+ * Date:    2015-01-06
+ * Copyright (C) Sabre Inc
+ */
+public class CpuIntensiveAnalyzerOne extends CpuIntensiveAnalyzerBase {
+}
